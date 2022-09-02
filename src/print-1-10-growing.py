4cd0d383b5014f30bf782d5456c875364338005f
Genona Torruella <genona.torruella@gmail.com>
2022-09-02 15:02:10 +0200

    changes

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,2 +1,8 @@
 
 # Print the numbers described in the exercise
+for n in range(1,11):
+    for i in range(1,n):
+        if(i==n-1):
+            print(i)
+        else:
+            print(i,end= ' ')
