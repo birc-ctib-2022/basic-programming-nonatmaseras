ba71f34897775f3266d6cc438adb1c026b2ed3f5
Genona Torruella <genona.torruella@gmail.com>
2022-09-05 17:18:20 +0200

    password

--- src/password.py
+++ src/password.py
@@ -8,5 +8,15 @@ password = sys.argv[1]
 is_valid = False
 
 # Do all the requirement checks here.
+for c in password:
+    if(c.islower()):
+        lower = 1
+    if(c.isupper()):
+        upper = 1
+    if c in "$#@":
+        special_c = 1
+
+if (lower == 1 & upper == 1 and special_c==1 and len(password)>= 6 and len(password)<=12):
+    is_valid= True
 
 print(is_valid)
